b210034579dcc2ce4895a34b270cab82ef210f64
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-02 16:21:29 +0530

    Add iterative postorder traversal implementation
    Implemented an iterative postorder traversal using a modified Morris traversal approach.

--- Trees/PostorderTraversal/postorderTraversal.cpp
+++ Trees/PostorderTraversal/postorderTraversal.cpp
@@ -24,3 +24,66 @@ public:
         return res;
     }
 };
+
+#if 0
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while (!st.empty()) {
+            TreeNode* node = st.top(); st.pop();
+            res.push_back(node->val);
+
+            if (node->left) st.push(node->left);
+            if (node->right) st.push(node->right);
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        std::vector<int>res;
+        TreeNode* curr = root;
+
+        while(curr)
+        {
+            if(!curr->right)
+            {
+                res.push_back(curr->val);
+                curr = curr->left;
+            }
+            else
+            {
+                TreeNode* prev = curr->right;
+                while(prev->left && prev->left!=curr)
+                {
+                    prev = prev->left;
+                }
+                if(!prev->left){
+                    res.push_back(curr->val);
+                    prev->left = curr;
+                    curr=curr->right;
+                }else{
+                    prev->left = nullptr;
+                    curr = curr->left;
+                }
+
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+#endif
