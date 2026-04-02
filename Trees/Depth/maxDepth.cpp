d73fdf01d2d13a9158138ad55d7b99d793548f03
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-02 17:46:20 +0530

    Implement maxDepth function using iterative approach

--- Trees/Depth/maxDepth.cpp
+++ Trees/Depth/maxDepth.cpp
@@ -9,7 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        std::stack<std::pair<TreeNode*, int>>rootDepth;
+        rootDepth.push({root, 1});
 
+        int maxAns = 0;
+
+        while(!rootDepth.empty())
+        {
+            auto[curr, currMax] = rootDepth.top();
+            rootDepth.pop();
+
+            maxAns = std::max(maxAns, currMax);
+            if(curr->left) rootDepth.push({curr->left, currMax+1});
+            if(curr->right) rootDepth.push({curr->right, currMax+1});
+        }
+        return maxAns;
+    }
+};
+#if 0
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -20,3 +41,4 @@ public:
 
     }
 };
+#endif
