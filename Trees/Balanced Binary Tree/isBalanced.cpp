f52043a5794dfa478a053be35d3c1c09f7bc57e7
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-07 13:10:25 +0530

    Implement iterative isBalanced function
    Added an alternative implementation for checking if a binary tree is balanced using an iterative approach with a stack.

--- Trees/Balanced Binary Tree/isBalanced.cpp
+++ Trees/Balanced Binary Tree/isBalanced.cpp
@@ -27,3 +27,41 @@ public:
         return isBalanced(root->left) && isBalanced(root->right);
     }
 };
+
+
+#if 0
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+        stack<TreeNode*>st;
+        unordered_map<TreeNode*, int> height;
+        TreeNode* curr = root;
+        TreeNode* visitedNode = nullptr;
+        while(curr || !st.empty())
+        {
+            if(curr)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }else
+            {
+                auto node = st.top();
+                if(node->right && node->right != visitedNode){
+                    curr = node->right;
+                }else {
+                    int left = node->left ? height[node->left] : 0;
+                    int right = node->right ? height[node->right] : 0;
+
+                    if(abs(left-right)>1) return false;
+
+                    height[node] = 1 + max(left, right);
+                    visitedNode = node;
+                    st.pop();
+                }
+            }
+        }
+        return true;
+    }
+};
+#endif
