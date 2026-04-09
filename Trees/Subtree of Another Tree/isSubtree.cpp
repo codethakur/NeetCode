c6e6f4f37380fc5901f8e5d1640c4db37d1eb000
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-09 10:55:55 +0530

    Add isSubtree function for binary tree comparison
    Implement isSubtree function to check if one tree is a subtree of another.

---
+++ Trees/Subtree of Another Tree/isSubtree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+        bool isSame(TreeNode* p, TreeNode* q) {
+        stack<pair<TreeNode*, TreeNode*>> st;
+        st.push({p, q});
+
+        while (!st.empty()) {
+            auto [n1, n2] = st.top();
+            st.pop();
+
+            if (!n1 && !n2) continue;
+            if (!n1 || !n2) return false;
+            if (n1->val != n2->val) return false;
+
+            st.push({n1->left, n2->left});
+            st.push({n1->right, n2->right});
+        }
+        return true;
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root) return false;
+
+        if (isSame(root, subRoot)) return true;
+
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+    }
+};
