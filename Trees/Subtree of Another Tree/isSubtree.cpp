d7525b3784a942a202469b97ebce4f939c6354d3
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-09 11:49:26 +0530

    Comment out alternative isSubtree implementation
    Commented out an alternative implementation of isSubtree using serialization.

--- Trees/Subtree of Another Tree/isSubtree.cpp
+++ Trees/Subtree of Another Tree/isSubtree.cpp
@@ -38,3 +38,26 @@ public:
                isSubtree(root->right, subRoot);
     }
 };
+#if 0
+
+class Solution {
+    void serialize(TreeNode* root, string& s) {
+         if (!root) {
+            s += "#,";   // null marker
+            return;
+        }
+
+        s += to_string(root->val) + ",";
+        serialize(root->left, s);
+        serialize(root->right, s);
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        string s1, s2;
+        serialize(root, s1);
+        serialize(subRoot, s2);
+
+        return s1.find(s2) != string::npos;
+    }
+};
+#enfif
