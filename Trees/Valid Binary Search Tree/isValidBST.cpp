49bde00f3cae7b1f581a85182c9bf96688b488bf
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-15 23:08:38 +0530

    Implement iterative isValidBST function
    Added an iterative approach for validating a BST using a queue.

--- Trees/Valid Binary Search Tree/isValidBST.cpp
+++ Trees/Valid Binary Search Tree/isValidBST.cpp
@@ -26,3 +26,32 @@ public:
         return isTrue(root, LONG_MIN, LONG_MAX);
     }
 };
+#if 0
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+
+        queue<tuple<TreeNode*, long, long>> queue;
+        queue.push(make_tuple(root, LONG_MIN, LONG_MAX));
+
+        while (!queue.empty()) {
+            auto [node, left, right] = queue.front();
+            queue.pop();
+
+            if (!(left < node->val && node->val < right)) {
+                return false;
+            }
+            if (node->left) {
+                queue.push(make_tuple(node->left, left, node->val));
+            }
+            if (node->right) {
+                queue.push(make_tuple(node->right, node->val, right));
+            }
+        }
+
+        return true;
+    }
+};
