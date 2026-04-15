4a5b71a8c508877d8b06114083e0033ca3558bce
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-15 22:19:50 +0530

    Implement goodNodes function with BFS approach
    Added an alternative implementation of the goodNodes function using a queue for breadth-first traversal.

--- Trees/Count Good Nodes in Binary Tree/goodNodes.cpp
+++ Trees/Count Good Nodes in Binary Tree/goodNodes.cpp
@@ -26,3 +26,32 @@ public:
         return dfs(root, root->val);
     }
 };
+#if 0
+class Solution {
+
+public:
+    int goodNodes(TreeNode* root) {
+        int ans = 0;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, INT_MIN});
+
+        while(!q.empty())
+        {
+            auto[node, maxVal] = q.front();
+            q.pop();
+
+            if(node->val >= maxVal){
+                ans++;
+            }
+            if(node->left){
+                q.push({node->left, max(maxVal, node->val)});
+            }
+            if(node->right){
+                q.push({node->right, max(maxVal, node->val)});
+            }
+        }
+        return ans;
+    }
+};
+
+#endif
