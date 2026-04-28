89547d04067cceb4769872d8d24c0ac5ab7a8249
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-04-28 15:00:08 +0530

    Create Subsets.cpp

---
+++ Backtracking/Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    void dfs(vector<int>& nums,  vector<vector<int>>&res,vector<int>&subset, int i)
+    {
+        if(i==nums.size()){
+            res.push_back(subset);
+            return;
+        }
+        subset.push_back(nums[i]);
+        dfs(nums, res, subset, i+1);
+        subset.pop_back();
+        dfs(nums, res, subset, i+1);
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        vector<int>subset;
+
+        dfs(nums, res, subset, 0);
+        return res;
+    }
+};
