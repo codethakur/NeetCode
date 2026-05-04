87469f7de43f8d868a69ee1456ff3cc16e9fd995
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-05-04 14:25:28 +0530

    Update combinationSum2.cpp

--- Backtracking/Combination Sum II/combinationSum2.cpp
+++ Backtracking/Combination Sum II/combinationSum2.cpp
@@ -25,7 +25,7 @@ public:
             if(tgt<0 || i>=candidates.size()) continue;
 
             for(int j = candidates.size()-1; j>=i; j--)
-            {
+            {    
                 if(j>i && candidates[j] == candidates[j-1]) continue;
                 if(candidates[i]>tgt) continue;
 
@@ -38,3 +38,35 @@ public:
         return ans;
     }
 };
+
+#if 0
+class Solution {
+     void dfs(int i, vector<int>& candidates, int target,
+             vector<int>& temp, vector<vector<int>>& ans) {
+        if(target == 0){
+            ans.push_back(temp);
+            return;
+        }
+        for(int j = i; j<candidates.size(); j++)
+        {
+            if(j>i && candidates[j] == candidates[j-1]) continue;
+            if(candidates[j]>target) break;
+
+            temp.push_back(candidates[j]);
+            dfs(j+1, candidates, target-candidates[j], temp, ans);
+
+            temp.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        std::sort(candidates.begin(), candidates.end());
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+        dfs(0, candidates, target, temp, ans);
+        return ans;
+    }
+};
+
+#endif
