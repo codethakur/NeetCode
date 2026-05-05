a0186997e57c0c0825f58073d9ede843872e7f1a
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-05-05 15:28:00 +0530

    Implement DFS for generating permutations with backtrack

--- Backtracking/Permutations/permute.cpp
+++ Backtracking/Permutations/permute.cpp
@@ -1,3 +1,4 @@
+#if 0
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -37,5 +38,37 @@ public:
         }
         return ans;
 
+    }
+};
+#endif
+class Solution {
+    void dfs(vector<int>& nums, vector<bool>& used,
+         vector<int>& temp, vector<vector<int>>& ans) {
+
+    if (temp.size() == nums.size()) {
+        ans.push_back(temp);
+        return;
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (used[i]) continue;
+
+        used[i] = true;
+        temp.push_back(nums[i]);
+
+        dfs(nums, used, temp, ans);
+
+        temp.pop_back();      // backtrack
+        used[i] = false;
+    }
+}
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        vector<bool>used(nums.size(), false);
+        dfs(nums, used, temp, ans);
+        return ans;
+        
     }
 };
