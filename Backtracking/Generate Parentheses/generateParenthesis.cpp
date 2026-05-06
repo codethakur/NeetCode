02cf028d94e7261f12e01a6654b8980014ff7e94
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-05-06 13:32:44 +0530

    Implement DFS for generating parentheses Recursive version

--- Backtracking/Generate Parentheses/generateParenthesis.cpp
+++ Backtracking/Generate Parentheses/generateParenthesis.cpp
@@ -1,3 +1,4 @@
+#if 0
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
@@ -33,4 +34,29 @@ public:
 
     }
 };
+#endif
+class Solution {
+    void dfs(int open, int close, int n,string s, vector<string>& ans)
+    {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        // add '('
+        if (open < n)
+            dfs(open + 1, close, n, s + "(", ans);
+
+        // add ')'
+        if (close < open)
+            dfs(open, close + 1, n, s + ")", ans);
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        dfs(0, 0, n, "", ans);
+        return ans;
+    }
+};
+
  
