a04e516b79aa4c4631db3fb9a93a0d64d85a427c
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-05-06 12:17:26 +0530

    Implement unique permutations || with backtracking

--- Backtracking/Permutations II/permuteUnique.cpp
+++ Backtracking/Permutations II/permuteUnique.cpp
@@ -1,3 +1,4 @@
+#if 0
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
@@ -40,6 +41,49 @@ public:
                 st.push({nextTemp, nextUsed});
             }
         }
+        return ans;
+    }
+};
+#endif
+class Solution {
+    void dfs(vector<int>& nums,
+             vector<bool>& used,
+             vector<int>& temp,
+             vector<vector<int>>& ans)
+    {
+        if (temp.size() == nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (used[j]) continue;
+
+            // duplicate handling
+            if (j > 0 && nums[j] == nums[j - 1] && !used[j - 1])
+                continue;
+
+            temp.push_back(nums[j]);
+            used[j] = true;
+
+            dfs(nums, used, temp, ans);
+
+            temp.pop_back();
+            used[j] = false;
+        }
+    }
+
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<vector<int>> ans;
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+
+        dfs(nums, used, temp, ans);
+
         return ans;
     }
 };
