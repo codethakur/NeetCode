403e7559aca1f6d64b8eeedc6cadb036ade30986
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-05-09 12:04:01 +0530

    Update canPartitionKSubsets.cpp

--- Backtracking/Partition to K Equal Sum Subsets/canPartitionKSubsets.cpp
+++ Backtracking/Partition to K Equal Sum Subsets/canPartitionKSubsets.cpp
@@ -1,45 +1,78 @@
 class Solution {
 public:
+
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        if(k>nums.size()) return false;
+
+        if (k > nums.size())
+            return false;
 
         int sum = 0;
-        for(int x : nums)
-        {
+
+        for (int x : nums)
             sum += x;
-        }
-        if(sum%k != 0) return false;
 
-        int target = sum/k;
+        if (sum % k != 0)
+            return false;
 
-        std::sort(nums.rbegin(), nums.rend());
-        if(nums[0]>target) return false;
+        int target = sum / k;
 
-        struct Frame
-        {
+        sort(nums.rbegin(), nums.rend());
+
+        if (nums[0] > target)
+            return false;
+
+        struct Frame {
             int idx;
-            vector<int>temp;
+            vector<int> buckets;
         };
 
-        stack<Frame>st;
-        st.push({0, {}});
-        while(!st.empty())
-        {
-            auto[idx,  temp] = st.top();
+        stack<Frame> st;
+
+        st.push({0, vector<int>(k, 0)});
+
+        while (!st.empty()) {
+
+            auto [idx, buckets] = st.top();
             st.pop();
 
-            if(idx == nums.size()){
-                return true;
+            // all numbers placed
+            if (idx == nums.size()) {
+
+                bool ok = true;
+
+                for (int x : buckets) {
+                    if (x != target) {
+                        ok = false;
+                        break;
+                    }
+                }
+
+                if (ok)
+                    return true;
+
+                continue;
             }
 
-           for(int i = k; i>=0; i--)
-           {
-                if(i<k && temp[j] + temp[j-1] >target) continue;
+            // try placing nums[idx] into each bucket
+            for (int i = k - 1; i >= 0; i--) {
 
-                temp[j] = nums[idx];
-                st.push({idx+1, temp});
-           }
+                // exceeds target
+                if (buckets[i] + nums[idx] > target)
+                    continue;
+
+                // duplicate-state pruning
+                if (i < k - 1 &&
+                    buckets[i] == buckets[i + 1])
+                    continue;
+
+                vector<int> nextBuckets = buckets;
+
+                nextBuckets[i] += nums[idx];
+
+                st.push({idx + 1, nextBuckets});
+            }
         }
 
+        return false;
     }
 };
