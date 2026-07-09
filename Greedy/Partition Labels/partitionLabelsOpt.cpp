e97071d6ca0256ef9791467121c1f30c0295c5f8
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-07-09 12:15:52 +0530

    Create partitionLabelsOpt.cpp

---
+++ Greedy/Partition Labels/partitionLabelsOpt.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26, 0);
+
+        for(int i = 0; i < s.size(); i++)
+        {
+            last[s[i] - 'a'] = i;
+        }
+
+        std::vector<int>ans;
+        int start = 0;
+        int end = 0;
+
+        for(int i =0; i<s.size(); i++)
+        {
+            end = max(end, last[s[i] - 'a']);
+            if( i == end)
+            {
+                ans.push_back(end-start+1);
+                start = end+1;
+            }
+        }
+        return ans;
+
+    }
+};
