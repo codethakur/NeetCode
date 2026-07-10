596d519016d0f1637f0acdfdd635fa425f958aef
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2026-07-10 12:07:21 +0530

    Create checkValidStringOpt.cpp

---
+++ Greedy/Valid Parenthesis String/checkValidStringOpt.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkValidString(string s)
+    {
+        int low = 0;
+        int high = 0;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == '(')
+            {
+                low++;
+                high++;
+            }
+            else if(s[i] == ')')
+            {
+                low--;
+                high--;
+            }
+            else // s[i] == '*'
+            {
+                low--;   // treat '*' as ')'
+                high++;  // treat '*' as '('
+            }
+
+            if(high < 0)
+                return false;
+
+            if(low < 0)
+                low = 0;
+        }
+
+        return low == 0;
+    }
+};
